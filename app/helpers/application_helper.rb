bfe084ab5425f1759b6f1feb172c6c0e354a23f0
RomaoFilipe <romaofilipe1999@gmail.com>
2025-09-03 05:48:25 +0100

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,19 @@ module ApplicationHelper
     remaining_seconds = seconds % 60
     "#{hours}h #{remaining_minutes}m #{remaining_seconds}s"
   end
+
+  def safe_path(*candidates)
+    meth = candidates.find { |m| respond_to?(m) }
+    meth ? public_send(meth) : root_path
+  end
+
+  # Atalho para abrir a página de "Campos" já no separador Mapa
+  # Uso: fields_map_path => /fields?view=map
+  def fields_map_path
+    base = respond_to?(:fields_path) ? fields_path : root_path
+    uri  = URI.parse(base.to_s)
+    params = Rack::Utils.parse_nested_query(uri.query).merge('view' => 'map')
+    uri.query = params.to_query
+    uri.to_s
+  end
 end
