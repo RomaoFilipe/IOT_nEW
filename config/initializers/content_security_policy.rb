b0ee8f12ca444a6660af6e97fcdd150fe4766ae9
RomaoFilipe <romaofilipe1999@gmail.com>
2025-09-15 15:46:38 +0100

    fix

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -23,3 +23,19 @@
 #   # Report violations without enforcing the policy.
 #   # config.content_security_policy_report_only = true
 # end
+# config/initializers/content_security_policy.rb
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self
+    policy.font_src    :self, :data, "https://api.mapbox.com"
+    policy.img_src     :self, :data, "https://api.mapbox.com", "https://*.tiles.mapbox.com"
+    policy.script_src  :self, :unsafe_inline, "https://api.mapbox.com", "https://cdn.tailwindcss.com", "https://cdn.jsdelivr.net"
+    policy.style_src   :self, :unsafe_inline, "https://api.mapbox.com", "https://cdn.tailwindcss.com"
+    policy.connect_src :self, "https://api.mapbox.com", "https://events.mapbox.com", "https://*.tiles.mapbox.com"
+    policy.worker_src  :self, :blob
+    policy.frame_ancestors :self
+  end
+
+  # Em dev podes comentar o report_only para ver logo o bloqueio se houver
+  # config.content_security_policy_report_only = true
+end
